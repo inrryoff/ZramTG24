e340af0c91e07f3952a4804efe26cbd38f1b5479
Inrryoff <inrrymacollemcastroreis@gmail.com>
2026-05-08 23:37:28 -0300

    Add files via upload

--- service.sh
+++ service.sh
@@ -1,2 +1,7 @@
-sleep 45
+#!/system/bin/sh
+until [ "$(getprop sys.boot_completed)" -eq 1 ]; do sleep 5; done
+sleep 0.1
+
+sync && echo 3 > /proc/sys/vm/drop_caches
+
 sh /data/adb/modules/ZramTG24/ram.sh &
